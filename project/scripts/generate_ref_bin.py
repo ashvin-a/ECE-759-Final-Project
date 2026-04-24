82a9a0d42de364bba5602d00cdefec9fc0ccc9d8
ashvin <ashvinanilkumarsh@gmail.com>
2026-04-23 22:44:10 -0500

    Modify ref bin generation script.

--- project/scripts/generate_ref_bin.py
+++ project/scripts/generate_ref_bin.py
@@ -1,9 +1,8 @@
 import cv2, sys, os
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, SCRIPT_DIR)
 from hog_utils import extract_hog
 
+SCRIPT_DIR = os.path.dirname("project/results/")
 patch = cv2.imread(os.path.join(SCRIPT_DIR, "patch.png"))
 feat  = extract_hog(patch)
 out   = os.path.join(SCRIPT_DIR, "ref_feat.bin")
